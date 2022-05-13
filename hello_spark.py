fb693dfe1a5f34819fd23ff7cd21d353f4d0b5e6
Yuliia Martynenko <julia.faust1103@gmail.com>
2022-05-12 20:02:18 -0700

    Finished the set up of simple application

--- hello_spark.py
+++ hello_spark.py
@@ -1,5 +1,35 @@
-from pyspark.sql import *
+import sys
 
+from pyspark import SparkConf
+from pyspark.sql import *
+from lib.logger import Log4J
+from lib.utils import get_spark_app_config, load_survey_df, count_by_country
 
 if __name__ == "__main__":
-    print("Hello")
+
+    conf = get_spark_app_config()
+
+    spark = SparkSession.builder \
+        .config(conf=conf) \
+        .getOrCreate()
+
+    logger = Log4J(spark)
+
+    if len(sys.argv) != 2:
+        logger.error("Usage: HelloSpark <filename>")
+        sys.exit(-1)
+
+    logger.info("Starting HelloSpark")
+
+    # my code
+
+    survey_df = load_survey_df(spark, sys.argv[1])
+    partitioned_survey_df = survey_df.repartition(2)
+
+    count_df = count_by_country(partitioned_survey_df)
+
+    logger.info(count_df.collect())
+
+    logger.info("Finished HelloSpark")
+
+    spark.stop()
